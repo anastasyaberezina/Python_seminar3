1d1c5daf622737517124860a000ac332e534185b
Anastasiya B <gavrilovaanastasya@yandex.ru>
2022-12-08 13:28:53 +0300

    Составьте список чисел Фибоначчи, в том числе для отрицательных индексов

---
+++ HomeWork4.py
@@ -0,0 +1,25 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+num = int(input('Введите число '))
+
+def Fib(numb):
+    lst = []
+    x, y = 1, 1
+    for i in range(numb-1):
+        lst.append(x)
+        x=y
+        y+=x
+    x, y = 0, 1
+    for i in range(numb):
+        lst.insert(0, x)
+        x=y
+        y=x-y
+        return lst
+
+lst=Fib(num)
+lst2=lst[::-1]
+res=[-x for x in lst2]
+result_lst=res+Fib(num)
+print(result_lst)
+
+
