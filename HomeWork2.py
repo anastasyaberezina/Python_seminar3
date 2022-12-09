9e306e44c5b0e79aeddba4956330db1c52d09cca
Anastasiya B <gavrilovaanastasya@yandex.ru>
2022-12-09 09:45:56 +0300

    найдёт разницу между максимальным и минимальным значением дробной части элементов

--- HomeWork2.py
+++ HomeWork2.py
@@ -1,26 +1,27 @@
 # Задайте список из вещественных чисел. Напишите программу, 
 # которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
+# Хотела решить чере %1, что то не вышло.
 
 lst = [1.1, 1.2, 3.1, 5.1, 10.01]
 
-for i in range(len(lst)):
+for i in range(len(lst)): # Ищу дробную часть элеметов
     lst[i] = lst[i]%1
     res=round(lst[i], 2)
     print(res)
-    res=str(res)
 
-    if res == float:
-        
-
-        for j in range(len(res)):
-            min=res[0]
-            max=0
-            if res[j]>max:
-                max=res[j]
-                min=max
-            else:
-                max=max
-                min=res[j]
-        print(max,min)            
+if res==float:
+    for j in range(len(res)): # Запускаю поэлементный цикл 
+    
+        min=res[0]
+        print(round(res[0], 2))
+        max=0
+        if res[j]>max:
+            max=res[j]
+            min=max  
+        else:
+            max=max
+            min=res[j]
+        break
+    print(max-min)            
 
